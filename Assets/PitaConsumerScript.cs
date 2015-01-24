7f8465329c39b6b26fb2e6c1bf0d5c3b823efed3
Hanokh Aloni <hanokh.aloni@gmail.com>
2015-01-24 11:50:42 +0200

    Fixed score

---
+++ Assets/PitaConsumerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PitaConsumerScript : MonoBehaviour {
+
+	public float aggroRange;
+	public bool isAggro;
+	public PlayerUpdateLocation target;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		
+		Vector3 playerLocation = target.transform.position;
+		float distance = Vector3.Distance (currentPosition, playerLocation);
+		Vector3 moveToward;
+		isAggro = distance < aggroRange;
+		
+		if (isAggro) {
+			if (target.pitaCounter > 0)
+			{
+				target.decPitaCounter ();
+				ScoreBehavior.score ++;
+			}
+		}
+
+		
+	}
+}
---
+++ Assets/Scripts/ScoreBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreBehavior : MonoBehaviour {
+	// Use this for initialization
+	public static int score;
+	
+	Text _text;
+	
+	
+	void Awake () {
+		_text = GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_text.text = "Score " + score.ToString();
+	}
+}
+
